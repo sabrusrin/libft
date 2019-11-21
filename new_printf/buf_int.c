51cf711141325b064ec83ec3c192381dc03ce462
Cristopher Hermiston <chermist@at-o9.21-school.ru>
2019-11-21 18:41:10 +0300

    new branch

--- new_printf/buf_int.c
+++ new_printf/buf_int.c
@@ -6,7 +6,7 @@
 /*   By: chermist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 01:40:51 by chermist          #+#    #+#             */
-/*   Updated: 2019/11/21 17:14:53 by chermist         ###   ########.fr       */
+/*   Updated: 2019/11/21 18:39:39 by chermist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@ void	precinbr(t_pf *sup, t_vec *buf, t_vec *nbuf, int len)
 	int	i;
 
 	i = 0;
-	if (*(char*)ft_vat(nbuf, 0) == '-')
+	if (*(char*)ft_vat(nbuf, 0) == '-' || sup->plus == '+')
 		i = 1;
+	sup->preci -= ((len < sup->preci && !i) ? len : len - 1);
+	len += sup->preci;
+	if (sup->width > len)
+	{
+		sup->width -= len - 1;
+		put_width(buf, sup, 'R', ' ');
+	}
+	else if (sup->plus != '+' && sup->space == ' ')
+		ft_vpush_back(buf, " ", sizeof(char));
 	if (sup->sign == '-')
 		ft_vpush_back(buf, "-", sizeof(char));
 	else if (sup->plus == '+')
-	{
 		ft_vpush_back(buf, "+", sizeof(char));
-		i = 1;
-	}
-	sup->preci -= ((len < sup->preci && !i) ? len : len - 1);
-	len += sup->preci;
-	sup->width -= ((sup->width > len) ? (len - 1) : sup->width);
-	put_width(buf, sup, 'R', ' ');
 	while (sup->preci--)
 		ft_vpush_back(buf, "0", sizeof(char));
 }
@@ -55,14 +57,15 @@ void	putnbr_buf(t_pf *sup, t_vec *buf, t_vec *nbuf)
 		sup->width -= ((sup->width > wlen) ? (wlen - 1) : sup->width);
 		put_width(buf, sup, 'R', '0');
 	}
-	else
+	else if (sup->width > len)
 	{
 		sup->width -= ((sup->width > wlen) ? (wlen - 1) : sup->width);
 		put_width(buf, sup, 'R', ' ');
 		if (sup->plus == '+' && sup->sign == '+')
 			ft_vpush_back(buf, "+", sizeof(char));
 	}
-	len -= ((sup->plus == '+' && !i) ? 1 : 0);
+	else if (sup->plus != '+' && sup->space == ' ')
+		ft_vpush_back(buf, " ", sizeof(char));
 	while (i < len)
 		ft_vpush_back(buf, ft_vat(nbuf, i++), sizeof(char));
 	put_width(buf, sup, 'L', ' ');
